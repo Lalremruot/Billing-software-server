65e1ea66674765127fe883314343fee558a6b094
Lalremruot <lalremruotinfimate@gmail.com>
2025-11-14 15:21:46 +0530

    new

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 }));
